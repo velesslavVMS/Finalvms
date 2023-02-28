a64c5022bf496b246ba45e84b3fe2103ce74fb7e
Vedamir <grandros1@gmail.com>
2023-02-28 21:06:02 +0800

    вводим исходный массив

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите размер исходного массива: n=");
+int n = Convert.ToInt32(Console.ReadLine());  // вводим кол-во эл-тов массива
+//Console.Write("Введите элементы исходного массива ");
+string[] array = new string[n]; // вводим кол-во эл-тов массива
+for (int i = 0; i < n; i++)
+array[i] = Convert.ToString(Console.ReadLine());
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+
